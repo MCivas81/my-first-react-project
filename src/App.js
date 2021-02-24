a64f0a93511a2d17cdf0afc7a82194bba40e966e
Marco Civello <marco.civello@gmail.com>
2021-02-24 15:20:46 +0100

    added consola as devDependecie

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var today = new Date();
-  var date = today.getFullYear();
+  const today = new Date();
+  const date = today.getFullYear();
+
   return (
     <div className="App">
       <header className="App-header">
